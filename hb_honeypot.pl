ad2993984325d0caf37c40b9a40e6cdf95c07045
Jonathan <chapmajs@gmail.com>
2014-04-13 22:59:46 -0400

    Adding GPLv3 license to heartbleed honeypot

--- hb_honeypot.pl
+++ hb_honeypot.pl
@@ -10,9 +10,24 @@
 # Run as root for the privileged port. Outputs IPs of suspected heartbleed scan
 # to the console. Rickrolls scanner in the hex dump.
 #
-# 8 April 2014
-# http://www.glitchwrks.com/
 # shouts to binrev
+#
+# Copyright (c) 8 April 2014 The Glitch Works
+# http://www.glitchwrks.com/
+# Version 0.1.1
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
